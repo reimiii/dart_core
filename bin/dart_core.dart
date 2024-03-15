35e2f9645acd3ac2c795fc53ae1639bd9dc2eae7
Hilmi AM <imiia75775@proton.me>
2024-03-15 12:07:32 +0700

    Initial commit

---
+++ bin/dart_core.dart
@@ -0,0 +1,5 @@
+import 'package:dart_core/dart_core.dart' as dart_core;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_core.calculate()}!');
+}
---
+++ test/dart_core_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_core/dart_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
